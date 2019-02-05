d909ef2d402729540cefcc8554874b2885074445
Nishanth <nishanth.engg93@gmail.com>
2019-02-05 10:49:22 +0530

    * Write build script to create lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import {Badge, Button} from 'antd';
 import * as React from 'react';
 
 import './App.css';
 import HelpPage from './components/helpPage';
 
 class App extends React.Component {
+  constructor(props:any) {
+    super(props);
+  }
+
   public render() {
     return (
       <div className="App">
